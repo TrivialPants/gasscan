dc02daf5cf4168ac59dea1cd6d8ac9920a1de527
TannerThurman <wiltthur@ut.utm.edu>
2016-09-06 10:34:03 -0500

    GasScan_v1
    GasScan

---
+++ gasscan/app/src/main/java/com/example/tanner/gasscan/GasScanActivity.java
@@ -0,0 +1,21 @@
+package com.example.tanner.gasscan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GasScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gas_scan);
+    }
+
+
+
+
+
+
+
+
+}
